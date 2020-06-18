dcd826c12e53261222b2ed45d03a4ae542983f95
Kajonczyk <kopczackikamil2000@gmail.com>
2020-06-18 19:15:04 +0200

    Updates

---
+++ precache-manifest.1de03b57576a1cfae882cc8eab7d0a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bbaf8ad6e0bb1b46779bc8b2650d40",
+    "url": "/fontmanager/index.html"
+  },
+  {
+    "revision": "a5e917eec6ebf07bb08e",
+    "url": "/fontmanager/static/js/2.e185b33e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fontmanager/static/js/2.e185b33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c252777ee49bcd616d",
+    "url": "/fontmanager/static/js/main.1e21fcba.chunk.js"
+  },
+  {
+    "revision": "acb7cce66fa97883ce13",
+    "url": "/fontmanager/static/js/runtime-main.428773aa.js"
+  },
+  {
+    "revision": "ab3149353f921a1a25fd38e6c7f65fe7",
+    "url": "/fontmanager/static/media/Logo.ab314935.svg"
+  }
+]);
